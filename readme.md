900953c1303e2c92f7e59d9423eee347877dce53
guoxiolong <275520895@qq.com>
2025-06-03 15:48:46 +0000

    make this project can run

---
+++ readme.md
@@ -0,0 +1,43 @@
+
+# structure
+-  lite-server（a sever base on BrowserSync）,bs-config.json is its config file
+
+- truffle
+
+- -是一个以太坊开发框架。
+- -主要用于**编写、编译、部署和测试智能合约**。
+- -提供命令行工具（如 `truffle compile`、`truffle migrate`、`truffle test`）。
+- -管理合约的部署脚本和测试脚本。
+
+- ganache
+
+- - 是一个**本地以太坊区块链模拟器**。
+- - 用于本地开发和测试，模拟真实以太坊网络的行为。
+- - 启动后会生成一组测试账户和私钥，方便开发者进行合约部署和交互。
+- - 有命令行版（ganache-cli）和桌面版（Ganache GUI）。
+
+# how to run
+> 本地运行和ganache交互
+
+```
+npm install
+```
+
+```
+# 安装 Ganache CLI（命令行版)
+npm install -g ganache-cli
+# 安装完成后，启动本地以太坊节点
+ganache-cli --port 7545
+```
+另起来一个terminal窗口
+```
+# build contracts
+npx truffle compile
+# deploy contract to Ganache
+npx truffle migrate --network development
+```
+
+启动lite-server
+```
+npm run dev
+```
--- src/app.js
+++ src/app.js
@@ -1,57 +1,38 @@
 App = {
   loading: false,
+  local: true, 
   contracts: {},
 
   load: async () => {
-    await App.loadWeb3()
-    await App.loadAccount()
+    await App.loadWeb3()   
     await App.loadContract()
     await App.render()
   },
 
   // https://medium.com/metamask/https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
   loadWeb3: async () => {
-    if (typeof web3 !== 'undefined') {
-      App.web3Provider = web3.currentProvider
-      web3 = new Web3(web3.currentProvider)
+    if (!App.local && window.ethereum) {
+      console.log('MetaMask is installed!');
+      // 请求用户授权
+      let accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+      window.web3 = new Web3(window.ethereum);
+      console.log('Connected with account:', accounts[0]);
+      App.account = accounts[0];
     } else {
-      window.alert("Please connect to Metamask.")
-    }
-    // Modern dapp browsers...
-    if (window.ethereum) {
-      window.web3 = new Web3(ethereum)
-      try {
-        // Request account access if needed
-        await ethereum.enable()
-        // Acccounts now exposed
-        web3.eth.sendTransaction({/* ... */})
-      } catch (error) {
-        // User denied account access...
-      }
-    }
-    // Legacy dapp browsers...
-    else if (window.web3) {
-      App.web3Provider = web3.currentProvider
-      window.web3 = new Web3(web3.currentProvider)
-      // Acccounts always exposed
-      web3.eth.sendTransaction({/* ... */})
-    }
-    // Non-dapp browsers...
-    else {
-      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+      // 连接本地 Ganache
+      App.web3Provider = new Web3.providers.HttpProvider("https://symmetrical-lamp-xxv6655rp526xjq-7545.app.github.dev/")
+      web3 = new Web3(App.web3Provider)
+      console.log('Connected to local Ganache');
     }
   },
 
-  loadAccount: async () => {
-    // Set the current blockchain account
-    App.account = web3.eth.accounts[0]
-  },
+ 
 
   loadContract: async () => {
     // Create a JavaScript version of the smart contract
     const todoList = await $.getJSON('TodoList.json')
     App.contracts.TodoList = TruffleContract(todoList)
-    App.contracts.TodoList.setProvider(App.web3Provider)
+    App.contracts.TodoList.setProvider(window.web3.currentProvider)
 
     // Hydrate the smart contract with values from the blockchain
     App.todoList = await App.contracts.TodoList.deployed()
